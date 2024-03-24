85d276d5923010409dfa2403eb9a5b5c1afb98f6
Виктория Погиба <viktoruapogiba@gmail.com>
2024-03-24 17:55:31 +0300

    Добавьте файлы проекта.

---
+++ WPF-LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPF_LoginForm.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=LAPTOP-CMTQU0P6\\MSSQLSERVER2;Database=MVVMLoginDb;Trusted_Connection=True;";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
